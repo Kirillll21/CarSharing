31d472b572e59145121dcc9ed4d08613f4f9d919
Kirill Radancev <radanzev2004@gmail.com>
2023-12-25 12:13:09 +0300

    Add project files.

---
+++ CarSharing/AuthPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarSharing
+{
+    /// <summary>
+    /// Interaction logic for AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var user = DbConnect.entObj.Users.FirstOrDefault(x=>x.Login == txbLogin.Text && x.Password == psbPassword.Password);
+
+                if (user == null) 
+                {
+                    MessageBox.Show("Такого пользователя нет!",
+                                    "Ошибка входа",
+                                    MessageBoxButton.OK,
+                                    MessageBoxImage.Information);
+
+                }
+                else 
+                {
+                    switch (user.IdRole)
+                    {
+                        case 1:
+                            MessageBox.Show("Добро пожаловать!",
+                                            "Вход",
+                                            MessageBoxButton.OK,
+                                            MessageBoxImage.Information);
+                            break;
+                        case 2:
+                            MessageBox.Show("Добро пожаловать!",
+                                            "Вход",
+                                            MessageBoxButton.OK,
+                                            MessageBoxImage.Information);
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ CarSharing/ListCars.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarSharing
+{
+    /// <summary>
+    /// Interaction logic for ListCars.xaml
+    /// </summary>
+    public partial class ListCars : Page
+    {
+        public ListCars()
+        {
+            InitializeComponent();
+            DgrCars.ItemsSource = DbConnect.entObj.Cars.ToList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            FrameApp.frmObj.GoBack();
+        }
+    }
+}
